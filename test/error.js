51c7bd940b6201dd50abce5eab4c8007cf479b55
Stephan Hohe <sth.dev@tejp.de>
2018-06-06 21:55:38 +0000

    Test with ava

--- test/error.js
+++ test/error.js
@@ -1,7 +1,7 @@
+import test from 'ava';
 
-import StreamWatcher from "../src/streamwatcher";
+import StreamWatcher from "../lib/streamwatcher";
 
-import assert from 'assert';
 import stream from 'stream';
 
 class ChunkReader extends stream.Readable {
@@ -11,174 +11,240 @@ class ChunkReader extends stream.Readable {
 	}
 	_read(size) {
 		while (this.chunks.length) {
-			if (!this.push(this.chunks.shift()))
+			const chunk = this.chunks.shift();
+			if (chunk instanceof Function) {
+				if (!chunk(this))
+					return;
+			}
+			else if (!this.push(chunk)) {
 				return;
+			}
 		}
 		this.push(null);
 	}
 };
 
-class NullWritable extends stream.Writable {
+class NullWriter extends stream.Writable {
 	_write(c, e, cb) { cb(); }
 };
 
-async function assertFulfill(p) {
-	const ret = await p;
-	assert.ok(true);
-	return ret;
+
+function assert_pending(promise) {
+	return new Promise((resolve, reject) => {
+		promise.then(
+			() => { reject(new Error("already fulfilled")); },
+			() => { reject(new Error("already rejected")); }
+		);
+		setTimeout(() => { resolve("still pending"); }, 200);
+	});
 }
 
-async function assertReject(p, error, message) {
-	let ret;
-	try {
-		ret = await p;
-	}
-	catch (err) {
-		assert.ok(true);
-		return err;
-	}
-	assert.throws(() => { return ret; }, error, message);
-}
 
-describe("watcher with no streams", function() {
-	beforeEach(function () {
-		this.watcher = new StreamWatcher();
-	});
+// helpers
 
-	it("fulfills", async function() {
-		await assertFulfill(this.watcher.finish);
-	});
+test("assert_pending()", async t => {
+	await t.notThrows(assert_pending(new Promise(() => {})));
+	await t.throws(assert_pending(Promise.resolve()));
+	await t.throws(assert_pending(Promise.reject()));
+	//await t.throws(Promise.reject());
 });
 
 
-it("watcher with readable stream fulfills on end of stream", async function() {
-	this.watcher = new StreamWatcher();
-	this.src = new ChunkReader(["abc", "def"]);
-	this.watcher.watch(this.src);
-	while (this.src.read() !== null)
-		;
-	await assertFulfill(this.watcher.finish);
-	assert.ok(true);
-});
+// no streams
 
-describe("watcher with single stream", function() {
-	beforeEach(function() {
-		this.watcher = new StreamWatcher();
-		this.src = new stream.PassThrough();
-		this.watcher.watch(this.src);
-	});
-
-	it("fulfills on end of stream", async function() {
-		this.src.write("abc");
-		this.src.end("def");
-
-		await assertFulfill(this.watcher.finish);
-	});
-
-	it("rejects on stream error", async function() {
-		this.src.write("abc");
-		this.src.emit("error", "E");
-
-		const err = await assertReject(this.watcher.finish);
-		assert.equal(err, "E");
-	});
+test("watcher without streams fulfills", async t => {
+	const watcher = new StreamWatcher();
+	await t.notThrows(watcher.finish);
 });
 
 
-describe("watcher with {error: ...}", function() {
-	beforeEach(function() {
-		this.watcher = new StreamWatcher();
-		this.src = new stream.PassThrough();
-	});
+// readable streams
 
-	it("rejects with modified error", async function() {
-		this.watcher.watch(this.src, {
-			error(err) { return err + "-custom"; }
-		});
+test("stays pending for readable stream while stream isn't read", async t => {
+	const watcher = new StreamWatcher();
+	const src = new ChunkReader(["abc", "def"]);
+	
+	const psrc = watcher.watch(src);
 
-		this.src.write("abc");
-		this.src.emit("error", "E");
-
-		const err = await assertReject(this.watcher.finish);
-		assert.equal(err, "E-custom");
-	});
-
-	it("doesn't reject with ignored error", async function() {
-		this.watcher.watch(this.src, {
-			error(err) { return; }
-		});
-
-		this.src.write("abc");
-		this.src.emit("error", "E");
-		this.src.end("def");
-
-		await assertFulfill(this.watcher.finish);
-	});
+	// As long as `src` isn't read stream/watcher shouldn't finish
+	await t.notThrows(assert_pending(psrc), "stream still pending");
+	await t.notThrows(assert_pending(watcher.finish), "watcher stillp pending");
 });
 
-describe("watcher for two piped streams", function() {
-	beforeEach(function() {
-		this.watcher = new StreamWatcher();
-		this.src = new stream.PassThrough();
-		this.dest = new NullWritable();
-		this.src.pipe(this.dest);
-		this.watcher.watch(this.src);
-		this.watcher.watch(this.dest);
-	});
+test("fulfills for readable stream when stream ends", async t => {
+	const watcher = new StreamWatcher();
+	const src = new ChunkReader(["abc", "def"]);
+	
+	const psrc = watcher.watch(src);
 
-	it("fulfills on completed pipe", async function() {
-		this.src.end("abc");
+	// Read whole stream by piping it to a writer
+	src.pipe(new NullWriter());
 
-		await assertFulfill(this.watcher.finish);
-	});
-
-	it("fulfills on completed pipe with multiple targets", async function() {
-		this.dest2 = new NullWritable();
-		this.src.pipe(this.dest2);
-
-		this.watcher.watch(this.dest2);
-
-		await assertFulfill(this.watcher);
-	});
-
-	it("rejects on error in pipe source", async function() {
-		this.src.emit('error', "a");
-		const res = await assertReject(this.watcher.finish);
-		assert.equal(res, "a");
-	});
-
-	it("rejects on error in pipe destination", async function() {
-		this.dest.emit("error", "b");
-		const err = await assertReject(this.watcher.finish);
-		assert.equal(err, "b");
-	});
-
-	it("rejects on successful source and error in pipe destination", async function() {
-		this.src.write("abc");
-		this.dest.emit("error", "b");
-		this.src.end("def");
-		const err = await assertReject(this.watcher.finish);
-		assert.equal(err, "b");
-	});
+	await t.notThrows(psrc, "stream finished without error");
+	await t.notThrows(watcher.finish, "watcher finished without error");
 });
 
-it("watcher only resolves after all streams are finished", async function() {
-	this.src1 = new stream.PassThrough();
-	this.src2 = new stream.PassThrough();
-	this.watcher = new StreamWatcher();
-	this.watcher.watch(this.src1);
-	this.watcher.watch(this.src2);
+test("rejects for readable stream when error occurs", async t => {
+	const watcher = new StreamWatcher();
+	const src = new ChunkReader(["abc", s => s.emit("error", new Error("E"))]);
+	
+	const psrc = watcher.watch(src);
 
-	this.src1.end("s1");
+	// Read whole stream by piping it to a writer
+	src.pipe(new NullWriter());
 
-	let done = false;
+	await t.throws(psrc, "E", "stream finished with error");
+	await t.throws(watcher.finish, "E", "watcher finished with error");
+});
 
-	setTimeout(() => {
-		assert.ok(!done);
-		this.src2.end("s2");
-	}, 100);
 
-	await this.watcher.finish;
-	done = true;
-	assert.ok(true);
+// writable streams
+
+test("stays pending for writable stream while stream isn't complete", async t => {
+	const watcher = new StreamWatcher();
+	const dest = new NullWriter();
+	
+	const pdest = watcher.watch(dest);
+
+	// As long as `src` isn't read stream/watcher shouldn't finish
+	await t.notThrows(assert_pending(pdest), "stream still pending");
+	await t.notThrows(assert_pending(watcher.finish), "watcher still pending");
+});
+
+test("fulfills for writable stream when stream finishes", async t => {
+	const watcher = new StreamWatcher();
+	const dest = new NullWriter();
+	
+	const pdest = watcher.watch(dest);
+
+	// Write to stream
+	dest.write("abc");
+	dest.end("def");
+
+	await t.notThrows(pdest, "stream finished without error");
+	await t.notThrows(watcher.finish, "watcher finished without error");
+});
+
+test("rejects for writable stream when error occurs", async t => {
+	const watcher = new StreamWatcher();
+	const dest = new NullWriter();
+	
+	const pdest = watcher.watch(dest);
+
+	// Write to stream
+	dest.write("abc");
+	dest.emit("error", new Error("E"));
+	dest.end("def");
+
+	await t.throws(pdest, "E", "stream finished with error");
+	await t.throws(watcher.finish, "E", "watcher finished with error");
+});
+
+test("watcher with {error: ...} rejects with modified error", async t => {
+	const watcher = new StreamWatcher();
+	const dest = new NullWriter();
+
+	const pdest = watcher.watch(dest, {
+		error(err) { return new Error(err.message + "-custom"); }
+	});
+
+	dest.write("abc");
+	dest.emit("error", new Error("E"));
+
+	await t.throws(pdest, "E-custom");
+	await t.throws(watcher.finish, "E-custom");
+});
+
+test("watcher with {error: ...} doesn't reject with ignored error", async t => {
+	const watcher = new StreamWatcher();
+	const dest = new NullWriter();
+
+	const pdest = watcher.watch(dest, {
+		error(err) { return; }
+	});
+
+	dest.write("abc");
+	dest.emit("error", new Error("E"));
+	dest.end("def");
+
+	await t.notThrows(pdest);
+	await t.notThrows(watcher.finish);
+});
+
+test("fulfills on completed pipe", async t => {
+	const watcher = new StreamWatcher();
+	const src = new stream.PassThrough();
+	const dest = new NullWriter();
+
+	const psrc = watcher.watch(src);
+	const pdest = watcher.watch(dest);
+
+	src.pipe(dest);
+	src.write("abc");
+	src.end("def");
+
+	await t.notThrows(psrc);
+	await t.notThrows(pdest);
+	await t.notThrows(watcher.finish);
+});
+
+test("fulfills on completed pipe with multiple targets", async t => {
+	const watcher = new StreamWatcher();
+	const src = new stream.PassThrough();
+	const dest1 = new NullWriter();
+	const dest2 = new NullWriter();
+
+	const psrc = watcher.watch(src);
+	const pdest1 = watcher.watch(dest1);
+	const pdest2 = watcher.watch(dest2);
+
+	src.pipe(dest1);
+	src.pipe(dest2);
+	src.write("abc");
+	src.end("def");
+
+	await t.notThrows(psrc);
+	await t.notThrows(pdest1);
+	await t.notThrows(pdest2);
+	await t.notThrows(watcher.finish);
+});
+
+test("rejects on error in pipe source", async t => {
+	const watcher = new StreamWatcher();
+	const src = new stream.PassThrough();
+	const dest = new NullWriter();
+
+	const psrc = watcher.watch(src);
+	const pdest = watcher.watch(dest);
+
+	src.pipe(dest);
+	src.write("abc");
+	src.emit("error", new Error("E"));
+
+	await t.throws(psrc, "E");
+	await t.notThrows(assert_pending(pdest));
+	await t.throws(watcher.finish, "E");
+});
+
+test("rejects on error in pipe destination", async t => {
+	const watcher = new StreamWatcher();
+	const src = new stream.PassThrough();
+	const dest = new NullWriter();
+
+	const psrc = watcher.watch(src);
+	const pdest = watcher.watch(dest);
+
+	src.pipe(dest);
+	src.write("abc");
+	dest.emit("error", new Error("E"));
+
+	await t.notThrows(assert_pending(psrc));
+	await t.throws(pdest, "E");
+	await t.throws(watcher.finish, "E");
+
+	src.end("def");
+
+	await t.notThrows(psrc);
+	await t.throws(pdest, "E");
+	await t.throws(watcher.finish, "E");
 });
