96ad5d7ad5478ce8b423b4b19548b8e98235fdf2
Stephan Hohe <sth.dev@tejp.de>
2018-06-14 23:17:54 +0000

    Clean up StreamState implementation

--- src/state.js
+++ src/state.js
@@ -9,15 +9,43 @@ function untracked(promise) {
 	promise.catch(err => {});
 }
 
+class FailStates {
+	constructor() {
+		this._states = {};
+	}
+	_state(name) {
+		if (!this._states[name]) {
+			this._states[name] = {
+				triggered: false,
+				value: undefined,
+				actions: []
+			};
+		}
+		return this._states[name];
+	}
+	on(name, action) {
+		const st = this._state(name);
+		if (st.triggered)
+			action(st.value);
+		else
+			st.actions.push(action);
+	}
+	trigger(name, value) {
+		const st = this._state(name);
+		if (st.triggered)
+			return;
+		st.triggered = true;
+		st.value = value;
+		for (const action of st.actions) {
+			action(value);
+		}
+	}
+}
+
 export class StreamState {
 	constructor(stream, options = {}) {
 		this.stream = stream;
-
-		this._failureActions = {
-			error: [],
-			end: [],
-			finish: []
-		};
+		this._failures = new FailStates();
 
 		this.stream.on("error", err => {
 			if (options.error) {
@@ -25,23 +53,23 @@ export class StreamState {
 					err = options.error(err);
 				}
 				catch (handlerErr) {
-					this._triggerFailure("error", handlerErr);
+					this._failures.trigger("error", handlerErr);
 					return;
 				}
 				if (err) {
-					this._triggerFailure("error", err);
+					this._failures.trigger("error", err);
 					return;
 				}
 			} else {
-				this._triggerFailure("error", err);
+				this._failures.trigger("error", err);
 				return;
 			}
 		});
 		this.stream.on("end", () => {
-			this._triggerFailure("end", new EventNotFound());
+			this._failures.trigger("end", new EventNotFound());
 		});
 		this.stream.on("finish", () => {
-			this._triggerFailure("finish", new EventNotFound());
+			this._failures.trigger("finish", new EventNotFound());
 		});
 
 		this.end = this.eventState("end");
@@ -49,7 +77,7 @@ export class StreamState {
 		this.complete = new Promise((resolve, reject) => {
 			this.stream.on('end', resolve);
 			this.stream.on('finish', resolve);
-			this._failureActions.error.push(reject);
+			this._failures.on("error", reject);
 		});
 		this.close = this.eventState("close");
 
@@ -59,28 +87,15 @@ export class StreamState {
 		untracked(this.close);
 	}
 
-	_triggerFailure(eventName, value) {
-		const actions = this._failureActions[eventName];
-		
-		// Future failure actions are triggered immediately
-		this._failureActions[eventName] = {
-			push(action) { action(value); }
-		};
-
-		for (const action of actions) {
-			action(value);
-		}
-	}
-
 	eventState(eventName) {
 		return new Promise((resolve, reject) => {
 			this.stream.on(eventName, resolve);
 			if (eventName !== "error")
-				this._failureActions.error.push(reject);
+				this._failures.on("error", reject);
 			if (eventName !== "end" && eventName !== "close")
-				this._failureActions.end.push(reject);
+				this._failures.on("end", reject);
 			if (eventName !== "finish" && eventName !== "close")
-				this._failureActions.finish.push(reject);
+				this._failures.on("finish", reject);
 		});
 	}
 }
