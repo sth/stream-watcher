9ab48fa0fc97dc75d94ce52f34aae8a2be5a2e97
Stephan Hohe <sth.dev@tejp.de>
2018-06-15 02:20:09 +0000

    Add original error message on unexpected rejection

--- test/_helpers.js
+++ test/_helpers.js
@@ -3,7 +3,7 @@ export function assert_pending(promise) {
 	return new Promise((resolve, reject) => {
 		promise.then(
 			() => { reject(new Error("already fulfilled")); },
-			() => { reject(new Error("already rejected")); }
+			err => { reject(new Error("already rejected" + (err && err.message ? ": " + err.message : ""))); }
 		);
 		setTimeout(() => { resolve("still pending"); }, 200);
 	});
